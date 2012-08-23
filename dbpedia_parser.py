cc73e01e363ee312c9bf5757904bca1c641609e6
parvez <parvez.islam@sap.com>
2012-08-23 13:28:31 -0400

    updated

--- dbpedia_parser.py
+++ dbpedia_parser.py
@@ -1,3 +1,4 @@
+"""have to install the dbpediakit from https://github.com/ogrisel/dbpediakit"""
 import dbpediakit as dbk
 import dbk.archive
 
@@ -8,6 +9,4 @@ tuples = dbk.archive.extract_text(archive_file)
 
 first = tuples.next()
 first.id
-
 first.text[:60] + u"..."
-u'Autism is a disorder of neural development characterized by ...'
