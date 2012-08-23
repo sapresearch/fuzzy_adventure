f651ad15225612a96c57ffa1425733d838d2bf31
parvez <parvez.islam@sap.com>
2012-08-23 16:44:45 -0400

    update

--- dbpedia_parser.py
+++ dbpedia_parser.py
@@ -1,11 +1,12 @@
 """have to install the dbpediakit from https://github.com/ogrisel/dbpediakit"""
 import dbpediakit as dbk
-import dbk.archive
+import dbpediakit.archive
 
 archive_file = dbk.archive.fetch("peopledata")
 archive_file
 
 tuples = dbk.archive.extract_text(archive_file)
+tuples
 
 first = tuples.next()
 first.id
