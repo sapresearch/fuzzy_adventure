09648865983d064b30a65178674b1c2876f0efb1
pmiller10 <pmiller987@gmail.com>
2012-08-27 15:04:32 -0400

    Use extract_triples method and forked version of dbpediakit

--- dbpedia_parser.py
+++ dbpedia_parser.py
@@ -1,18 +1,16 @@
 """have to install the dbpediakit from https://github.com/ogrisel/dbpediakit"""
+import sys
+sys.path.append('./external/dbpediakit')
 import dbpediakit as dbk
 import dbpediakit.archive
 
 def search(keyword1 = "test1", keyword2 = "test2"):
     archive_file = dbk.archive.fetch("persondata")
-    #archive_file
-
-    tuples = dbk.archive.extract_text(archive_file)
-    #tuples
-
+    tuples = dbk.archive.extract_triple(archive_file)
     for record in tuples:
         arr = [keyword1, keyword2]
         for keyword in arr:
-            if record.title.find(keyword) != -1:
+            if record.id.find(keyword) != -1:
                 arr.remove(keyword);
-                if record.text.find(arr[0]) != -1:
+                if record.title.find(arr[0]) != -1:
                     print record
