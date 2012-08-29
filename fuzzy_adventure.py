f1a5ed745831cb62bc8a5b42fde4e10de5117dda
parvez <parvez.islam@sap.com>
2012-08-29 17:18:04 -0400

    added  new method to find the truth value of a sentence

--- fuzzy_adventure.py
+++ fuzzy_adventure.py
@@ -1,3 +1,4 @@
 import dbpedia_parser as dp
 
-dp.search('Bennelong', 'died')
+#dp.search('Bennelong', 'died') #third parameter is optional
+dp.search('Tol_Avery', 'Avery', 'surname') #only returns true or false (we are returning the data row for now
