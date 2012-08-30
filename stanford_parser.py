33f3043812e8ad8f4c5514ee76f66553344eb4fd
parvez <parvez.islam@sap.com>
2012-08-30 16:46:30 -0400

    new class for stamfor parser

---
+++ stanford_parser.py
@@ -0,0 +1,36 @@
+class Stamford_Node():
+    def __init__(self, string):
+        self.string = string
+
+    def __init__(self, type, word, position, probability, child = None, parent = None):
+        self.word = x
+        self.child = y
+        self.position = position
+        self.probability = probability
+        self.child = child
+        self.parent = parent
+
+    def parseString(self):
+        
+        
+    def getType(self):
+        return self.type
+
+    def getWord(self):
+        return self.word
+
+    def wordIndex():
+        return self.position
+
+    def getProbability():
+        return self.probability
+
+    def children():
+        return
+
+    def siblings():
+        return
+
+    def depth():
+        return
+        
