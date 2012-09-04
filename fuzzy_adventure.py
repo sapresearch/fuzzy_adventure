aa98b698a09f6728eed88c9b7466e259ba1f7e38
parvez <parvez.islam@sap.com>
2012-09-04 13:47:49 -0400

    update

--- fuzzy_adventure.py
+++ fuzzy_adventure.py
@@ -1,4 +1,9 @@
 import dbpedia_parser as dp
+import stanford_parser as sp
 
 #dp.search('Bennelong', 'died') #third parameter is optional
-dp.search('Tol_Avery', 'Avery', 'surname') #only returns true or false (we are returning the data row for now
+#dp.search('Tol_Avery', 'Avery', 'surname') #only returns true or false (we are returning the data row for now
+
+str = '(ROOT (SBARQ [82.494] (WHNP [3.854] (WP [2.692] What)) (SQ [76.704] (VP [76.641] (ADVP [16.203] (RB [15.877] salesperson)) (VBD [4.662] made) (NP [48.802] (NP [13.505] (DT [0.650] the) (JJS [1.444] most) (NNS [4.188] sales)) (PP [34.891] (IN [1.850] in) (NP [32.639] (DT [0.650] the) (NNP [6.213] United) (NNP [9.167] States) (JJ [3.925] last) (NN [5.153] quarter)))))) (. [0.004] ?)))'
+arr = sp.parseString(str)
+print arr
--- stanford_parser.py
+++ stanford_parser.py
@@ -1,17 +1,14 @@
-class Stamford_Node():
-    def __init__(self, string):
-        self.string = string
-
-    def __init__(self, type, word, position, probability, child = None, parent = None):
-        self.word = x
-        self.child = y
-        self.position = position
+class StamfordNode():
+    depth = 0
+    siblings = []
+    
+    def __init__(self, type = 'ROOT', word = None, index = 1, probability = 0.0, children = [], parent = None):
+        self.type = type
+        self.word = word
+        self.index = index
         self.probability = probability
-        self.child = child
+        self.children = children
         self.parent = parent
-
-    def parseString(self):
-        
         
     def getType(self):
         return self.type
@@ -26,11 +23,52 @@ class Stamford_Node():
         return self.probability
 
     def children():
-        return
+        return children
 
     def siblings():
         return
 
     def depth():
         return
-        
+
+def parseString(tree):
+    start = '('
+    close = ')'
+    brac = '['
+    node_arr = []
+    word_count = 0
+    depth = 0
+    #siblings
+
+    arr = tree.split()
+    stack = []
+    par_node = 1
+    #print curr_node
+    for element in arr:
+        #print curr_node
+        if element[0] == start or element[0] == brac:
+            stack.append(element)
+            par_node = par_node + 1
+        elif element[-1] == close:
+            count = element.count(close)
+            w = element[:-count]
+            #print w
+            p = ''
+            t = ''
+            word_count = word_count + 1
+            i = word_count
+            for j in range(count):
+                #print "range " + str(range(count))
+                #print j
+                ele = stack.pop()
+                print ele
+                if ele[0] == brac:
+                    j = j + 1
+                    p = element[1:-1]
+                    #print p
+                elif ele[0] == start:
+                    t = element[1:]
+                    #print t
+                    node_arr.append(StamfordNode(t, w, i, p, [], par_node))
+                    #print "par "
+    return node_arr
