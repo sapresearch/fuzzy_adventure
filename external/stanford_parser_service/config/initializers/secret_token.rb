bad01dcf909cbdd244d3d573018492f118d79531
pmiller10 <pmiller987@gmail.com>
2012-09-19 09:20:56 -0400

    Delete jruby server

--- external/stanford_parser_service/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-StanfordParserService::Application.config.secret_token = 'e732954c078649e1d4b87efa7a0cee4c25cfd3ff2b455078da0e2d855588fccd2eea601e5e31c5864c9e8c2c5254129dd5584b879856a7b6365a097d3e9af7ce'
