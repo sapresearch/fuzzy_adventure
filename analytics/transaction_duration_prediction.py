cf08550c86d11659e4fdd1c1bc5ba2a1689b46e7
atremblay <alexis.tremblay@sap.com>
2012-12-11 10:29:22 -0500

    Made the vectorizing of transaction more general

--- analytics/transaction_duration_prediction.py
+++ analytics/transaction_duration_prediction.py
@@ -15,39 +15,69 @@ import NN_weights_persistence
 
 
 
-priorities = ['Very high', 'High', 'Medium', 'Low']
-statuses = ['Confirmed', \
-'Completed', \
-'In Process', \
-'New', \
-'Author Action', \
-'Completed for Author', \
-'Partner Action']
-
+priorities = []
+statuses = []
+programmers = []
+contract_priorities = []
+products = []
 
 
 
 def get_base_informations():
+	global priorities, statuses, programmers, contract_priorities, products
 	"""
 	Obtain the basic informations needed from the database for the duration prediction
 	"""
-	db = MySQLdb.connect(host="localhost", user="root", passwd="", db="batcave")
+	db = MySQLdb.connect(host="localhost", user="root", passwd="nolwen", db="watchTower")
 
-	db.query("""SELECT * FROM transactions""")
-	transactions = db.store_result().fetch_row(0)
+	db.query("""SELECT * FROM transactions LIMIT 100000""")
+	transactions = db.store_result().fetch_row(0,1)
 	print "\n%d transactions loaded from the database." % len(transactions)
 
-	db.query("""SELECT * FROM messages""")
-	messages = db.store_result().fetch_row(0)
-
 	db.query("""SELECT * FROM components""")
-	raw_components = db.store_result().fetch_row(0)
-	components = [component[1] for component in raw_components]
+	rows = db.store_result().fetch_row(0,1)
+	components = [row['name'] for row in rows]
 	
-
+	db.query("""SELECT * FROM programmers""")
+	rows = db.store_result().fetch_row(0,1)
+	programmers = [row['name'] for row in rows]
+	print "%d programmers" % len(programmers)
+	
+	priorities = priority_set(db)
+	print "%d priority levels" % len(priorities)
+	statuses = status_set(db)
+	print "%d different status" % len(statuses)
+	contract_priorities = contract_priority_set(db)
+	print "%d contract priority levels" % len(contract_priorities)
+	products = product_set(db)
+	print "%d different products" % len(products)
 	db.close()
 	
-	return transactions, messages, components
+	return transactions, components
+	
+
+def priority_set(db):
+	db.query("""SELECT priority FROM transactions""")
+	rows = db.store_result().fetch_row(0,1)
+	return list(set([row['priority'] for row in rows]))
+
+
+def status_set(db):
+	db.query("""SELECT status FROM transactions""")
+	rows = db.store_result().fetch_row(0,1)
+	return list(set([row['status'] for row in rows])	)
+	
+	
+def contract_priority_set(db):
+	db.query("""SELECT contract_priority FROM transactions""")
+	rows = db.store_result().fetch_row(0,1)
+	return list(set([row['contract_priority'] for row in rows]))
+
+	
+def product_set(db):
+	db.query("""SELECT product FROM transactions""")
+	rows = db.store_result().fetch_row(0,1)
+	return list(set([row['product'] for row in rows]))
 	
 	
 def stem_components(components):
@@ -60,15 +90,29 @@ def stem_components(components):
 	print "%d component families in the database" % len(component_set)
 	return list(component_set)
 		
-		
+	
+def vectorize_programmer(transaction):
+	"""
+	Return a np array containing all zeros except for the transaction's programmer.
+	This array act as a classifier. Only the programmer of the transaction is equal to one,
+	all other programmers are zeros.
+	"""
+	vector = np.zeros(len(programmers))
+	programmer = transaction['priority']
+	for i in range(len(programmers)):
+		vector[i] = int(programmers[i] == programmer)
+	return vector
+
+
 def vectorize_priority(transaction):
 	"""
 	Return a np array containing all zeros except for the transaction's priority.
 	This array act as a classifier. Only the priority of the transaction is equal to one,
 	all other priorities are zeros.
 	"""
+
 	vector = np.zeros(len(priorities))
-	priority = transaction[9]
+	priority = transaction['priority']
 	for i in range(len(priorities)):
 		vector[i] = int(priorities[i] == priority)
 	return vector
@@ -81,7 +125,7 @@ def vectorize_status(transaction):
 	all other statuses are zeros.
 	"""
 	vector = np.zeros(len(statuses))
-	status = transaction[11]
+	status = transaction['status']
 	for i in range(len(statuses)):
 		vector[i] = int(statuses[i] == status)
 	return vector
@@ -94,18 +138,44 @@ def vectorize_component(transaction):
 	all other components are zeros.
 	"""
 	vector = np.zeros(len(stemmed_components))
-	component_id = transaction[12] - 1
-	#raw_input(component_id)
+	component_id = transaction['component_id'] - 1
+
 	component = components[component_id]
 
-	#raw_input(component)
+
 	component = component.split('-')[0]
-	#raw_input(component)
+
 	for i in range(len(stemmed_components)):
 		vector[i] = int(stemmed_components[i] == component)
 	return vector
 		
-		
+
+def vectorize_contract_priority(transaction):
+	"""
+	Return a np array containing all zeros except for the transaction's contract priorities.
+	This array act as a classifier. Only the contract priority of the transaction is equal to one,
+	all other contract priorities are zeros.
+	"""
+	vector = np.zeros(len(contract_priorities))
+	contract_priority = transaction['contract_priority']
+	for i in range(len(contract_priorities)):
+		vector[i] = int(contract_priorities[i] == contract_priority)
+	return vector
+	
+	
+def vectorize_product(transaction):
+	"""
+	Return a np array containing all zeros except for the transaction's product.
+	This array act as a classifier. Only the product of the transaction is equal to one,
+	all other contract products are zeros.
+	"""
+	vector = np.zeros(len(products))
+	product = transaction['product']
+	for i in range(len(products)):
+		vector[i] = int(products[i] == product)
+	return vector
+	
+	
 def build_transaction_features(transaction):
 	"""
 	Build the transaction's features. This is where new features must be added.
@@ -115,34 +185,21 @@ def build_transaction_features(transaction):
 	features = np.append(features, vectorize_priority(transaction))
 	features = np.append(features, vectorize_status(transaction))
 	features = np.append(features, vectorize_component(transaction))
+	features = np.append(features, vectorize_programmer(transaction))
+	features = np.append(features, vectorize_contract_priority(transaction))
+	features = np.append(features, vectorize_product(transaction))
+	
+	len(features)
 	return features
 
-	
-def get_message_chain(transaction):
-	"""
-	Returns all the messages associated with a transaction as an array
-	"""
-	messages_chain = []
-	first_message_id = int(transaction[14])
-	if first_message_id == -1:
-		return messages_chain
-	
-	next_message_id = first_message_id
-	while next_message_id is not None:
-		next_message = messages[next_message_id - 1]
-		messages_chain.append(next_message)
-		next_message_id = next_message[5]
-		
-	return messages_chain
 
-
-def get_messages_time_span(messages):
+def transaction_duration(transaction):
 	"""
 	Based on a list of messages, returns the time elapsed between the first and the last one.
 	"""
-	first_message = messages[0]
-	last_message = messages[-1]
-	return (last_message[3] - first_message[3]).days + 1
+	start_date = transaction['start_date']
+	end_date = transaction['end_date']
+	return (end_date - start_date).days + 1
 
 	
 def ridgeCV(data, targets):
@@ -182,19 +239,25 @@ def extract_data_from_transactions(transactions):
 	The features transactions 2D array is of dimension nbTranscations X nbFeatures
 	The targets array is of dimension nbTransactions X 1
 	"""
+
 	featured_transactions = []
 	targets = []
-	#transactions = [t for t in transactions if len(get_message_chain(t)) > 0]
 
+	nb_transactions = len(transactions)
+	modulo = nb_transactions / 100
+	count = 0;
 	for transaction in transactions:
 		features = build_transaction_features(transaction)
-		
-		# Add the array of features of that transaction to the test set
-		featured_transactions.append(np.array(features))
-		targets.append(get_messages_time_span(get_message_chain(transaction)))
-		
+		#print "\t%d%% completed\r" % (count / modulo),
+		# Add the array of features of that transaction to the set
+		count += 1
+		featured_transactions.append(features)
+		targets.append(transaction_duration(transaction))
+	
+	
 	featured_transactions = np.array(featured_transactions)
 	targets = np.array(targets)
+	
 	
 	return featured_transactions, targets
 	
@@ -231,14 +294,18 @@ def negative_predictions(predictions):
 	return predictions[predictions < 0]
 
 
-transactions, messages, components = get_base_informations()
+transactions, components = get_base_informations()
 stemmed_components = stem_components(components)
+
 # Filter the transactions that have no messages.
 # This is done only because the messages are used to determine the duration of a transaction
 # If we had any other way of assuming the duration, we wouldn't have to filter.
-transactions = [t for t in transactions if len(get_message_chain(t)) > 0]
+transactions = [t for t in transactions if t['end_date'] is not None]
 
 featured_transactions, targets = extract_data_from_transactions(transactions)
+
+print "%d featured transactions" % len(featured_transactions)
+print "%d features for a transaction" % len(featured_transactions[0])
 
 size = 0.7
 training_size = int(len(featured_transactions) * size)
@@ -251,47 +318,48 @@ test_data = featured_transactions[-test_size:]
 test_targets = targets[-test_size:]
 print "%d transactions in the test set" % len(test_data)
 
-"""
+start = time.time()
 ridge_model = ridgeCV(training_data, training_targets)
-lasso_model = lasso(training_data, training_targets)
-linear_regression_model = linear_regression(training_data, training_targets)
+print "Took %s seconds to complete ridge model with %d training examples" %(time.time() - start, len(training_data))
+#lasso_model = lasso(training_data, training_targets)
+#linear_regression_model = linear_regression(training_data, training_targets)
 
 print "\nRidge score: %f" % score(ridge_model, test_data, test_targets)
-print "Lasso score: %f" % score(lasso_model, test_data, test_targets)
-print "Linear score: %f" % score(linear_regression_model, test_data, test_targets)
+#print "Lasso score: %f" % score(lasso_model, test_data, test_targets)
+#print "Linear score: %f" % score(linear_regression_model, test_data, test_targets)
 
 ridge_predictions = predictions(ridge_model, test_data)
-lasso_predictions = predictions(lasso_model, test_data)
-linear_regression_predictions = predictions(linear_regression_model, test_data)
+#lasso_predictions = predictions(lasso_model, test_data)
+#linear_regression_predictions = predictions(linear_regression_model, test_data)
 
 	
 ridge_mse = mean_squared_error(ridge_predictions, test_targets)
-lasso_mse = mean_squared_error(lasso_predictions, test_targets)
-linear_regression_mse = mean_squared_error(linear_regression_predictions, test_targets)
+#lasso_mse = mean_squared_error(lasso_predictions, test_targets)
+#linear_regression_mse = mean_squared_error(linear_regression_predictions, test_targets)
 
 print "\nMean Squared Error using ridge = %f. Best alpha is %f" % (ridge_mse, ridge_model.best_alpha)
-print "Mean Squared Error using lasso = %f" % lasso_mse
-print "Mean Squared Error using linear regression = %f" % linear_regression_mse
+#print "Mean Squared Error using lasso = %f" % lasso_mse
+#print "Mean Squared Error using linear regression = %f" % linear_regression_mse
 
 
 
 neg_ridge = negative_predictions(ridge_predictions)
 print "\n%d negative values in ridge predictions" % len(neg_ridge)
-neg_lasso = negative_predictions(lasso_predictions)	
-print "%d negative values in lasso predictions" % len(neg_lasso)
-neg_linear = negative_predictions(linear_regression_predictions)	
-print "%d negative values in linear regression predictions" % len(neg_linear)
+#neg_lasso = negative_predictions(lasso_predictions)	
+#print "%d negative values in lasso predictions" % len(neg_lasso)
+#neg_linear = negative_predictions(linear_regression_predictions)	
+#print "%d negative values in linear regression predictions" % len(neg_linear)
 
 
 ridge_mse_bench = mean_squared_error(np.ones(len(test_data)), test_targets)
-lasso_mse_bench = mean_squared_error(np.ones(len(test_data)), test_targets)
-lin_reg_mse_bench = mean_squared_error(np.ones(len(test_data)), test_targets)
+#lasso_mse_bench = mean_squared_error(np.ones(len(test_data)), test_targets)
+#lin_reg_mse_bench = mean_squared_error(np.ones(len(test_data)), test_targets)
 	
 print "\nOnes bench using ridge = %f" % ridge_mse_bench
-print "Ones bench using lasso = %f" % lasso_mse_bench
-print "Ones bench using linear regression = %f" % lin_reg_mse_bench
-"""
+#print "Ones bench using lasso = %f" % lasso_mse_bench
+#print "Ones bench using linear regression = %f" % lin_reg_mse_bench
 
+"""
 print "\n", "~" * 50
 print "\tNEURAL NETWORK"
 print "~" * 50
@@ -316,3 +384,4 @@ NN_weights_persistence.dump(net.params)
 #f.write("Real value:" % test_targets[0])
 f.write("NN took %f seconds with a trainUntilConvergence()\n" % (time.time()-start))
 f.close()
+"""
