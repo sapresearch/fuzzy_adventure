c5474e6e272d3f6414ef8b610e68ea9764a96855
pmiller10 <pmiller987@gmail.com>
2013-01-08 09:20:10 -0500

    Improved RAE

--- query_decomposition/rae.py
+++ query_decomposition/rae.py
@@ -8,6 +8,7 @@ from load_data import load_data
 from rnn import RNN
 import question_type
 from numpy import *
+import time
 
 
 class RAE(object):
@@ -15,7 +16,7 @@ class RAE(object):
 	def __init__(self, in_dims, out_dims):
 		self.dataset = UnsupervisedDataSet(in_dims)
 		cfg = RbmGibbsTrainerConfig()
-		cfg.maxIter = 3
+		cfg.maxIter = 5
 		self.model = Rbm.fromDims(in_dims, out_dims)
 		self.trainer = RbmBernoulliTrainer(self.model, self.dataset, cfg)
 
@@ -49,7 +50,7 @@ class SentenceRAE(RAE):
 		self._train(iterations)
 	
 	def train(self, iterations):
-		self.train_with_pairs(iterations)
+		self.train_with_pairs(5)
 		self.train_with_all(iterations)
 
 	def train_with_all(self, iterations):
@@ -65,9 +66,12 @@ class SentenceRAE(RAE):
 		tokens = [t for t in tokens if t != ' ' and t != '']
 		vectors = []
 		for s in tokens:
-			word_vector = self.wordspace[s] if s in self.wordspace else [0.001] * self.in_dims
+			word_vector = self.wordspace[s] if s in self.wordspace else [0.001] * (self.in_dims/2)
 			vectors.append(word_vector)
 		sentence_vector, all_vectors, concatenated = self.merge(vectors, [], [])
+		#print sentence_vector
+		if len(sentence_vector) == 0 or len(concatenated) == 0:
+			print "'" + sentence + "'"
 		return sentence_vector[0], concatenated[0]
 
 	def merge(self, array, total=[], total_originals=[]):
@@ -127,29 +131,81 @@ class RAEClassifier():
 		vector, _ = self.rae.activate(item)
 		return self.model.predict(vector)
 		
+	def test(self, test_data, test_targets):
+		preds = []
+		for q in test_data:
+			preds.append(self.predict(q)[0])
+		index = range(len(test_data))
+		correct = 0.
+		for i in index:
+			pred = preds[i]
+			target = test_targets[i]
+			correct = correct + 1. if pred == target else correct
+		print "Correct: " + str(correct/float(len(test_targets)))
 
 
 
+import nltk
+brown = nltk.corpus.brown
 
-questions, _, targets = load_data("../test/test_data.txt")
-_, word_vectors = question_type.train(questions, targets)
+sents = brown.sents()
+formatted = []
+dummy_targets = [] # So that the question_type.train function doesn't complain when it tries to train a RandomForest.
+for s in sents[0:500]:
+	s = [token for token in s if token not in ['?', ',', '.', '(', ')']]
+	if len(s) > 1:
+		sent = ' '.join(s)
+		formatted.append(sent)
+		dummy_targets.append('a')
+brown = formatted
+print "Training sentences: " + str(len(brown))
+#brown = ' '.join(brown)
+#brown = brown.split('. ')
+#brown = [s for s in brown if s != ' ' and s != '' and len(s) > 11]
+
+questions, _, targets = load_data("../test/lat_data.txt") # training 
+test_questions, _, test_targets = load_data("../test/test_data.txt")# cross validaiotn
+questions = [q.strip('?') for q in questions]
+test_questions = [q.strip('?') for q in test_questions]
+
+#_, word_vectors = question_type.train(questions, targets)
+_, word_vectors = question_type.train(brown, dummy_targets)
 for word,vector in word_vectors.items():
 	vector = list(vector)
 	word_vectors[word] = vector
 
-rae = SentenceRAE(40,20, word_vectors, questions)
-rae.train(0)
+
+rae = SentenceRAE(200,100, word_vectors, brown)
+epochs = 10
+rae.train(1)
+for i in range(epochs):
+	start = time.time()
+	
+	cls1 = RAEClassifier(rae, questions, targets)
+	cls2 = RAEClassifier(rae, test_questions, test_targets)
+	
+	print "Epochs: " + str(i+1) + "\n"
+	print "Classifier trained on training data"
+	cls1.test(questions, targets)
+	cls1.test(test_questions, test_targets)
+	
+	print "\nClassifier trained on cv data"
+	cls2.test(questions, targets)
+	cls2.test(test_questions, test_targets)
+
+	rae._train(1)
+	print "Time: " + str(time.time() - start) + "\n"
 
 
-cls = RAEClassifier(rae, questions, targets)
-print cls
-print questions[5]
-print cls.predict(questions[5])
-print questions[15]
-print cls.predict(questions[15])
-print questions[25]
-print cls.predict(questions[25])
-print questions[35]
-print cls.predict(questions[35])
-print questions[45]
-print cls.predict(questions[45])
+
+
+#print questions[5]
+#print cls.predict(questions[5])
+#print questions[15]
+#print cls.predict(questions[15])
+#print questions[25]
+#print cls.predict(questions[25])
+#print questions[35]
+#print cls.predict(questions[35])
+#print questions[45]
+#print cls.predict(questions[45])
