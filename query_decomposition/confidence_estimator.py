7ca6de51efaca0ad2f62ed76967a51796fd3ff31
atremblay <alexis.tremblay@sap.com>
2013-01-21 15:50:13 -0500

    Type programmer and component added

--- query_decomposition/confidence_estimator.py
+++ query_decomposition/confidence_estimator.py
@@ -1,21 +1,34 @@
 import re
+import MySQLdb as mdb
 
-
+db = mdb.connect(host="localhost", user="root", passwd="nolwen", db="batcave")
 
 def is_programmer(answer):
-    regex = re.compile("[\W\d_]+", re.UNICODE)
-    splits = re.split(regex, answer)
+    db.query("""SELECT * FROM programmers WHERE name = '%s'""" % answer)
+    result = db.store_result().fetch_row(0,1)
+
+    if len(result) == 1:
+        return True
+    else:
+        return False
+    """
+    regex = re.compile("-| ")
+    splits = re.split(regex, unicode(answer))
     print splits
     for split in splits:
         if not split.isalpha():
             return False
 
     return True
-
-
-
+    """
 
 
 def is_component(answer):
-    pass
+    db.query("""SELECT * FROM components WHERE name = '%s'""" % answer)
+    result = db.store_result().fetch_row(0,1)
+
+    if len(result) == 1:
+        return True
+    else:
+        return False
 
