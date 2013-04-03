2d0be467a9648bae9f05c64d74dbf4d4c0a21d79
atremblay <alexis.tremblay@sap.com>
2013-04-03 13:02:31 -0400

    Small update to parse_date to use command line

---
+++ parse_date.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from pandas import DataFrame, Series
+from datetime import datetime
+import sys
+
+def parse_date(dt):
+    _dt = None
+    try:
+        _dt = datetime.strptime(str(dt),"%Y%m%d%H%M%S")
+    except ValueError:
+        pass
+    return _dt
+
+args = sys.argv
+filename = args[1]
+df = pd.read_csv(filename)
+df['Closed On'] = df['Closed On'].apply(parse_date)
+df['Created On'] = df['Created On'].apply(parse_date)
+df['Changed On'] = df['Changed On'].apply(parse_date)
+
+i = df.columns
+i = i.map(str.upper)
+df.columns = i
+
+df.to_csv('parsed.csv', index=False)
