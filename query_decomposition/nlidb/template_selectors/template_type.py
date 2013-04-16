baccd2ea04773078bd6411068b45e2c8cd3a5f6b
atremblay <alexis.tremblay@sap.com>
2013-04-16 13:54:31 -0400

    Refactoring

--- query_decomposition/nlidb/template_selectors/template_type.py
+++ query_decomposition/nlidb/template_selectors/template_type.py
@@ -15,29 +15,12 @@ import os
 sys.path.append(os.environ['FUZZY_ADVENTURE'] + "/query_decomposition")
 from confidence_estimator import *
 class TemplateClassifier():
-    """
-    q1 = ("SELECT name, COUNT(transactions.programmer_id) AS close_count FROM programmers, transactions WHERE programmers.id = transactions.programmer_id AND programmers.name <>'' GROUP BY transactions.programmer_id ORDER BY close_count DESC LIMIT 1;", 0)
-    q2 = ('SELECT name, COUNT(transactions.component_id) FROM components INNER JOIN transactions on components.id = transactions.component_id GROUP BY transactions.component_id ORDER BY COUNT(transactions.component_id) DESC LIMIT 1;', 0)
-    q3 = ("SELECT COUNT(transactions.id) FROM transactions WHERE transactions.end_date IS NOT NULL AND transactions.programmer_id=(SELECT id FROM programmers WHERE programmers.name = '%s');", 1)
-    q4 = ("SELECT AVG(SECONDS_BETWEEN(start_date, end_date)) FROM transactions WHERE priority = '%s'", 1)
-    q5 = ("SELECT AVG(SECONDS_BETWEEN(start_date, end_date)) FROM transactions WHERE transactions.end_date IS NOT NULL AND transactions.programmer_id=(SELECT id FROM programmers WHERE programmers.name = '%s')", 1)
 
-    
-    q6 = ("SELECT COUNT(*) FROM TOUCHED WHERE PROGRAMMER_ID=%d", 1)
-    q7 = ("SELECT AVG(SECONDS_BETWEEN(start_date, end_date)) FROM transactions", 1)
-    q8 = ("SELECT AVG(MPT) FROM TRANSACTIONS", 0)
-    q9 = ("SELECT COUNT(*) FROM TRANSACTIONS WHERE %s='%s'", 2)
-    q10 = ("SELECT TRANS_NUMBER, COMPONENTS.NAME FROM TRANSACTIONS INNER JOIN COMPONENTS ON TRANSACTIONS.COMPONENT_ID = COMPONENTS.ID WHERE TRANSACTIONS.END_DATE IS NULL AND COMPONENTS.NAME = '%s'", 1)   
-    q11 = ("SELECT TRANS_NUMBER FROM TRANSACTIONS INNER JOIN PROGRAMMERS ON TRANSACTIONS.PROGRAMMER_ID = PROGRAMMERS.ID WHERE PROGRAMMERS.NAME IS NULL", 0)
-    """
-
-    
     q1 = ("SELECT name, COUNT(transactions.programmer_id) AS close_count FROM programmers, transactions WHERE programmers.id = transactions.programmer_id AND programmers.name <>'' GROUP BY transactions.programmer_id ORDER BY close_count DESC LIMIT 1;", ())
     q2 = ('SELECT name, COUNT(transactions.component_id) FROM components INNER JOIN transactions on components.id = transactions.component_id GROUP BY transactions.component_id ORDER BY COUNT(transactions.component_id) DESC LIMIT 1;', ())
     q3 = ("SELECT COUNT(transactions.id) FROM transactions WHERE transactions.end_date IS NOT NULL AND transactions.programmer_id=(SELECT id FROM programmers WHERE programmers.name = '%s');", (str,))
     q4 = ("SELECT AVG(SECONDS_BETWEEN(start_date, end_date)) FROM transactions WHERE priority = '%s'", (str,))
     q5 = ("SELECT AVG(SECONDS_BETWEEN(start_date, end_date)) FROM transactions WHERE transactions.end_date IS NOT NULL AND transactions.programmer_id=(SELECT id FROM programmers WHERE programmers.name = '%s')", (str,))
-
     q6 = ("SELECT COUNT(*) FROM TOUCHED WHERE PROGRAMMER_ID=%d", (int,))
     q7 = ("SELECT AVG(SECONDS_BETWEEN(start_date, end_date)) FROM transactions", ())
     q8 = ("SELECT AVG(MPT) FROM TRANSACTIONS", ())
@@ -46,13 +29,12 @@ class TemplateClassifier():
     q11 = ("SELECT TRANS_NUMBER FROM TRANSACTIONS INNER JOIN PROGRAMMERS ON TRANSACTIONS.PROGRAMMER_ID = PROGRAMMERS.ID WHERE PROGRAMMERS.NAME IS NULL", ())
 
 
-    templates = {'A': [q1, LAT.Programmer], 'B': [q2, LAT.Component], 'C': [q3, LAT.Integer], 'D': [q4, LAT.Decimal], 'E':[q5, LAT.Decimal], 'F': [q6, LAT.Unknown], 'G': [q7, LAT.Unknown],'H': [q8, LAT.Unknown],'I': [q9, LAT.Unknown],'J': [q10, LAT.Unknown],'K':[q11, LAT.Unknown]}
+    templates = {'A': q1, 'B': q2, 'C': q3, 'D': q4, 'E':q5, 'F': q6, 'G': q7,'H': q8,'I': q9,'J': q10,'K':q11}
 
     def __init__(self, model):
         self.model = model
     
     def template(self, query):
         klass = self.model.predict(query)
-        sql = self.templates[klass][0]
-        lat_type = self.templates[klass][1] # we aren't using this right now
+        sql = self.templates[klass]
         return sql, klass
