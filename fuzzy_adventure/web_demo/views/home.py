87f6576ee53f1a71fa020950b53fda47e7c2c2d3
Caitlin Mehl <mehlcat@gmail.com>
2013-05-26 11:58:21 -0400

    marginal update

--- fuzzy_adventure/web_demo/views/home.py
+++ fuzzy_adventure/web_demo/views/home.py
@@ -2,6 +2,7 @@ from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from fuzzy_adventure import to_sql
 
 
 def welcome(request):
@@ -10,5 +11,7 @@ def welcome(request):
         question = request.GET['question']
     except:
         question = ""
-    
-    return render_to_response("home.html", {}, context_instance=RequestContext(request))
+
+    answers = fuzzy_adventure.executable.to_sql(question)
+ 
+    return render_to_response("home.html", {'question': question, 'answer_ary': answers}, context_instance=RequestContext(request))
