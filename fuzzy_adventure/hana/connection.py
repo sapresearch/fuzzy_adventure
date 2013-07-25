37d693d97df48faba7122ae7863554d00bc31efe
atremblay <alexis.tremblay@sap.com>
2013-07-25 10:51:07 -0400

    Small modif to the queries and hana connection

--- fuzzy_adventure/hana/connection.py
+++ fuzzy_adventure/hana/connection.py
@@ -2,27 +2,29 @@ import pypyodbc as pdbc
 import getpass
 import sys
 
+cnxn = None
 def get_cursor():
+    global cnxn
     retry = True
     tries = 1
-    while retry and tries <= 5:
-        if not hasattr(get_cursor, "userName"):
-            get_cursor.userName = raw_input('User: ')
-        if not hasattr(get_cursor, "password"):
-            get_cursor.password = getpass.getpass('Password: ')
 
-        if not hasattr(get_cursor, "cnxn"):
+    if cnxn is None or cnxn.connected == 0:
+        while retry and tries <= 5:
+            if not hasattr(get_cursor, "userName"):
+                get_cursor.userName = raw_input('User: ')
+
+            if not hasattr(get_cursor, "password"):
+                get_cursor.password = getpass.getpass('Password: ')            
+
             try:
                 c = "DSN=hana;UID=%s;PWD=%s" % (str(get_cursor.userName), str(get_cursor.password))
-                get_cursor.cnxn = pdbc.connect(c)
+                cnxn = pdbc.connect(c)
                 retry = False
             except pdbc.Error as err:
+                print err.args
                 print 'Username and/or Password are incorrect'
                 delattr(get_cursor,"userName")
                 delattr(get_cursor,"password")
-        tries += 1
+            tries += 1
 
-    if not hasattr(get_cursor,"cnxn"): 
-        sys.exit('You failed 5 times to enter valid credentials. System exit.')
-
-    return get_cursor.cnxn.cursor()
+    return cnxn.cursor()
